3b3fd2e016f30ce6c818259c92fdb83d8b9079ab
Ammad Ul Islam <hammadch152@gmal.com>
2024-08-25 22:45:54 +0500

    Initial Commit

---
+++ FRUITS/FRUITSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  FRUITS
+//
+//  Created by Ammad Ul Islam Mac on 24/08/2024.
+//
+
+//import SwiftUI
+//
+//@main
+//struct FRUITSApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
